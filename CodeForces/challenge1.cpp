be02e28633c9b93615c83adc99c80adfee671b05
maahir22 <56473490+maahir22@users.noreply.github.com>
2020-03-20 00:10:10 +0530

    Create challenge1.cpp

---
+++ CodeForces/challenge1.cpp
@@ -0,0 +1,16 @@
+Problem link:https://codeforces.com/contest/1326/problem/A
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    while(n--){
+        int t;
+        cin>>t;
+        cout<<2;
+        for(int i=0;i<t-1;i++){
+            cout<<9;
+        }
+        cout<<endl;
+    }
+}
