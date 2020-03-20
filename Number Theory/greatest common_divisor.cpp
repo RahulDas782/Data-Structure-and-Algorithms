f14975259c34f14722a8a05de6012b9aacb9ad7d
maahir22 <56473490+maahir22@users.noreply.github.com>
2020-03-20 21:53:12 +0530

    Update greatest common_divisor.cpp

--- Number Theory/greatest common_divisor.cpp
+++ Number Theory/greatest common_divisor.cpp
@@ -13,23 +13,19 @@ long long  gcd_naive(long long a, long long b) {
   return current_gcd;
 }
 //this is an effecient approach for gcd by euclid's therem.
+//Derived from Euclid's Divison Lemma
+//If we are given two positive integers A,B then A= B*q+r;
+
 long long  euclidgcd(long long a,long long b)
 {
-//if b is zero as zero divides everything therefore a will be divided by gcd ie a only 
+//if b is zero as a divides 0 therefore a will be divided by gcd ie a only 
   if(b==0)
   {
     return a;
   }
-  else
-  {
-    long long a_prime = a%b;
-    //a recursive call as after only a few steps rather than naive solution , it will give b as a tiny zero which will make
+      //a recursive call as after only a few steps rather than naive solution , it will give b as a tiny zero which will make
     //if condition true .
-    return euclidgcd(b,a_prime);
-  }
-  
-
-
+    return euclidgcd(b,a%b);
 }
 
 
