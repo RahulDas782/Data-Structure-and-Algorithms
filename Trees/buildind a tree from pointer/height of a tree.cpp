7492c7ceab14e1d8d2b085ee3a21ef0d2a20840e
Kushagra Sharma <56391795+drunkeninja42@users.noreply.github.com>
2020-04-12 17:30:38 -0500

    simple height of a tree
    hey quick talk guys , subscribe to maahir sharma's livestream for google codejam's final

---
+++ Trees/buildind a tree from pointer/height of a tree.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+struct Bstnode{
+    int data;
+    Bstnode* left;
+    Bstnode* right;
+};
+
+Bstnode* GetNewNode(int data) {
+	Bstnode* newNode = new Bstnode();
+	newNode->data = data;
+	newNode->left = newNode->right = NULL;
+	return newNode;
+}
+
+int findmin(Bstnode* root)
+{
+    if(root==NULL)
+    {
+        cout<<"the tree is empty";
+        return -1;
+    }
+    else{
+        while(root->left!=NULL)
+        {
+            root = root->left;
+        }
+    }
+    return root->data;
+}
+Bstnode* Insert(Bstnode* root,int data) {
+	if(root == NULL) { // empty tree
+		root = GetNewNode(data);
+	}
+	// if data to be inserted is lesser, insert in left subtree. 
+	else if(data <= root->data) {
+		root->left = Insert(root->left,data);
+	}
+	// else, insert in right subtree. 
+	else {
+		root->right = Insert(root->right,data);
+	}
+	return root;
+}
+int findheight(Bstnode* root)
+{
+    if(root==NULL)
+    {
+        return -1;
+    }
+    else{
+        return max(findheight(root->left),findheight(root->right)) + 1;
+    }
+}
+int main() {
+	Bstnode* root = NULL;  // Creating an empty tree
+	root = Insert(root,15);	
+	root = Insert(root,10);	
+	root = Insert(root,20);
+	root = Insert(root,25);
+	root = Insert(root,8);
+	root = Insert(root,12);
+    cout<<findheight(root);
+}
+
