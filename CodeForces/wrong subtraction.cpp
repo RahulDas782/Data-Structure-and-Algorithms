dd869f357eb4348f6ef25e8af39474f11897f671
Kushagra Sharma <56391795+drunkeninja42@users.noreply.github.com>
2020-04-14 19:02:41 -0500

    one of many for igdtu

---
+++ CodeForces/wrong subtraction.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<set>
+#include<algorithm>
+#include<vector
+using namespace std;
+int main()
+{
+   unsigned long long int n;
+   int k;
+   cin>>n>>k;
+   while(k--){
+   if((n%10==0)
+   {
+       n =n%10;
+   }
+   else
+   {
+       n--;
+   }
+   cout<<n;
+   
+   }
+
+}
